6a4978965ea9ddb36131f9bbcecbc4d08ea39dd5
williamwutq <williamwutq@gmail.com>
2026-09-22 19:29:55 -0700

    [chore] Update criterion to 0.8

--- benches/locked_region.rs
+++ benches/locked_region.rs
@@ -1,5 +1,6 @@
 use bstack::BStack;
-use criterion::{BenchmarkId, Criterion, black_box, criterion_group, criterion_main};
+use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
+use std::hint::black_box;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::{Arc, Barrier};
 
